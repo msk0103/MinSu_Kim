80bd1305289e101223c065946c9adacf360ec82b
MinSu Kim <msk9200@gmail.com>
2015-07-06 10:38:04 +0900

    cummulative distrubution

--- Dice_Simulation/Dice_Simulation.R
+++ Dice_Simulation/Dice_Simulation.R
@@ -8,21 +8,33 @@ sum_dice <- dice1 + dice2
 
 # Plot your results for 10 experiments, 100 experiments and 10000 experiments
 
-dice_sample_10_1 <- sample(1:6, 10, replace = T)
-dice_sample_10_2 <- sample(1:6, 10, replace = T)
+# rolling dice for 10 experiments
+d <- 10
+dice1 <- sample(1:6, d, replace = T)
+dice2 <- sample(1:6, d, replace = T)
 
-hist(dice_sample_10_1 + dice_sample_10_2)
+sum_dice <- dice1 + dice2
 
-dice_sample_100_1 <- sample(1:6, 10^2, replace = T)
-dice_sample_100_2 <- sample(1:6, 10^2, replace = T)
+hist(sum_dice)
 
-hist(dice_sample_100_1 + dice_sample_100_2)
+# rolling dice for 100 experiments
+d <- 100
+dice1 <- sample(1:6, d, replace = T)
+dice2 <- sample(1:6, d, replace = T)
 
+sum_dice <- dice1 + dice2
 
-dice_sample_10000_1 <- sample(1:6, 10^4, replace = T)
-dice_sample_10000_2 <- sample(1:6, 10^4, replace = T)
+hist(sum_dice)
 
-hist(dice_sample_10000_1 + dice_sample_10000_2)
+# rolling dice for 10000 experiments
+d <- 10000
+dice1 <- sample(1:6, d, replace = T)
+dice2 <- sample(1:6, d, replace = T)
 
+sum_dice <- dice1 + dice2
 
-# What have you learned?
+hist(sum_dice)
+
+# cummulative distribution plot.
+
+plot.ecdf(sum_dice, main="cummulative distribution")
