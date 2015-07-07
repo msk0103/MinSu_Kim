b228f975fa34d9f702bed22e4607cc20a232eda2
MinSu Kim <msk9200@gmail.com>
2015-07-07 11:55:08 +0900

    central limit theory

---
+++ 20150707/Central_Limit_Theorm_Simulation.R
@@ -0,0 +1,23 @@
+
+# Rolling dice to test Central Limit Theorm Simulation
+for(i in 1:10000){
+  d <- 100
+  dice <- sample(1:6, d, replace = T)
+  dice_mean[i] <- mean(dice)
+}
+hist(dice_mean, freq = F)
+
+### Central Limit Theorm Simulation
+
+# uniform distribution
+hist(runif(10000)*10,main="")
+means <- numeric(10000)
+for (i in 1:10000){
+  means[i] <- mean(runif(5)*10)
+}
+hist(means,freq = F)
+mean(means)
+sd(means)
+xv <- seq(0,10,0.1)
+yv <- dnorm(xv, mean=mean(means),sd=sd(means))
+line(xv, yv)
